01d084b5b2984878d194b23e1ea87750a33623ce
Testoxe <testoxxe@gmail.com>
2019-11-10 19:33:47 +0100

    Added gestion des mots reserves

---
+++ cpc/MotReserve.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+class MotReserve
+{
+	std::string lexeme;
+	int uniteLexical;
+	
+public:
+	MotReserve(std::string lexeme, int uniteLexical);
+	std::string getLexeme();
+	int getUniteLexical();
+};
+
