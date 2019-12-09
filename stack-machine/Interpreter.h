9866098a5c55ac6f2470d4efecc21218fbc1ae07
Souhail <safi-bigg@live.fr>
2019-12-09 23:33:28 +0100

    Finilazing interpreter

--- stack-machine/Interpreter.h
+++ stack-machine/Interpreter.h
@@ -12,11 +12,12 @@ class Interpreter
 {
 private:
 	string inputFile; // File contain code
-	ifstream input;//peut être fichier,string dans la console..
-	std::vector<string> code;
+	vector<string> code;
 	ofstream output;
 	Memoire memoire;
+
 public:
+
 	string nextInstr(); // Returns next instruction
 	void exInstr(); // Execute next instruction
 	void codeFile(); // open file containing code
